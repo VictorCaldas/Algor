dcf1ae8c961cd6bccbc726d7988dd331f8588ae0
victorcaldas <contatos.caldas@gmail.com>
2018-02-06 17:15:21 -0200

    initial commit

---
+++ NumberRepeatedArray.go
@@ -0,0 +1,22 @@
+package main
+
+// thank you so much kio
+
+import (
+	"fmt"
+)
+
+func main() {
+	r := 0
+	b := [10] int { 1, 1, 4, 10, 5, 7, 7, 4, 10 }
+
+	for _, number := range b {
+		r = r ^ number
+	}
+
+	fmt.Println("Searching for number repeated in the array")
+	fmt.Println(b)
+	fmt.Println("")
+	fmt.Print("Number that repeats in array: ")
+	fmt.Print(r)
+}
